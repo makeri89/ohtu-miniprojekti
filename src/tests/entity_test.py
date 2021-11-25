b622b8953f0d187847ddd4c14b312eb2f13c9024
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-11-25 17:46:29 +0200

    Minimal ORM-Weblink created

--- src/tests/entity_test.py
+++ src/tests/entity_test.py
@@ -1,31 +1,9 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import unittest
 from entity import Weblink
 
-from flask_testing import TestCase
-
-#from app import app
-#from entity import db
-
 class TestWeblink(unittest.TestCase):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    TESTING = True
-    db = SQLAlchemy()    
-    def create_app(self):
-        app = Flask(__name__)
-
-        self.db.init_app(app)
-        app.app_context().push()
-        return app
-
     def setUp(self):
-        self.db = SQLAlchemy(self.create_app())
-        self.db.create_all()
-
-    def tearDown(self):
-        self.db.session.remove()
-        self.db.drop_all()
+        pass
 
     def test_weblink_has_name_and_url(self):
         weblink_entity = Weblink(title='example', url='http://example.com')
