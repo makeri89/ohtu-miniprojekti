420cff83d409b68a4370e33d261beeddb37db350
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-12-02 10:55:23 +0200

    Unit tests for Sprint 1 Finished

--- src/tests/entity_test.py
+++ src/tests/entity_test.py
@@ -11,4 +11,11 @@ class TestWeblink(unittest.TestCase):
         weblink_entity = weblink_service.create_weblink_for_testing(title='example', url='http://example.com')
         self.assertEqual(weblink_entity.title, 'example')
         self.assertEqual(weblink_entity.url, 'http://example.com')
+
+    def test_weblink_holds_integrity_in_and_out_of_database(self):
+        weblink_service.add_weblink('example1', 'http://example1.com')
+        #change below to fetch only specific weblink
+        all_weblinks = weblink_service.get_weblinks()
+        self.assertEqual(all_weblinks[-1].title, 'example1')
+        self.assertEqual(all_weblinks[-1].url, 'http://example1.com')
         
