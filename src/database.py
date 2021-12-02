65620d6583467c12db65a4b90293cf83dee4b074
Markus Kaihola <makeri89@hotmail.com>
2021-12-03 01:05:52 +0200

    add default db address

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app
 from config import DATABASE_URL
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL or \
+    'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
