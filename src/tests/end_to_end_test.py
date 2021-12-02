235ca14fafd2fbe8f037bcccbd5de4dd88804ea2
Markus Kaihola <makeri89@hotmail.com>
2021-12-03 01:20:48 +0200

    fix stupid error

--- src/tests/end_to_end_test.py
+++ src/tests/end_to_end_test.py
@@ -13,6 +13,6 @@ class TestEndToEnd(unittest.TestCase):
     def test_submitted_weblink_is_committed_to_database(self):
         requests.post('http://localhost:5000/sendvink', \
             data={'title': 'end_to_end_testing', 'url': 'http://end.to'})
-        results = requests.get('http://localhost:5000/weblinks')
+        results = requests.get('http://localhost:5000/')
         self.assertTrue(results.text.__contains__('end_to_end_testing'))
         
