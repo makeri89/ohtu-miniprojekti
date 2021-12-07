c2183c85d6b6f697d8dfce1498b29542286b2fb0
Markus Kaihola <markus.kaihola@helsinki.fi>
2021-12-07 02:00:39 +0200

    fixed pylint score

--- src/routes.py
+++ src/routes.py
@@ -7,28 +7,28 @@ weblink_service = WeblinkService()
 
 @app.route('/', methods=['GET'])
 def index():
-    weblinks = weblink_service.get_weblinks()
-    return render_template('index.html', weblinks=weblinks)
+    all_weblinks = weblink_service.get_weblinks()
+    return render_template('index.html', weblinks=all_weblinks)
 
 @app.route('/weblinks', methods=['GET', 'POST'])
 def weblinks():
     if request.method == 'GET':
-        weblinks = weblink_service.get_weblinks()
-        return render_template('index.html', weblinks=weblinks)
+        all_weblinks = weblink_service.get_weblinks()
+        return render_template('index.html', weblinks=all_weblinks)
     if request.method == 'POST':
         weblink_title = request.form['title']
         weblink_url = request.form['url']
         weblink_service.add_weblink(weblink_title, weblink_url)
         return redirect('/weblinks')
-    
+
 @app.route('/podcasts', methods=['GET', 'POST'])
 def podcasts():
     if request.method == 'GET':
-        podcasts = None
-        return render_template('index.html', podcasts=podcasts)
-    
+        all_podcasts = None
+        return render_template('index.html', podcasts=all_podcasts)
+
 @app.route('/books', methods=['GET', 'POST'])
 def books():
     if request.method == 'GET':
-        books = None
-        return render_template('index.html', books=books)
+        all_books = None
+        return render_template('index.html', books=all_books)
