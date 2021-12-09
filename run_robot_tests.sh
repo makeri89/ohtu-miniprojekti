d4f4c5846f1f0ebcd5b716afd65b1d5125c88022
Anssi Rajala <70325495+rajanssi@users.noreply.github.com>
2021-12-09 22:29:50 +0200

    Create run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:6000/ping)" != "200" ]]; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
   do sleep 1; 
 done
 
