bbe8a72cfccaefb504ce98e8f5090ab3d2c4da90
Anssi Rajala <anssi.rajala@helsinki.fi>
2021-12-09 22:57:26 +0200

    Update run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -2,7 +2,7 @@
 
 poetry run python3 src/index.py &
 
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/)" != "200" ]]; 
   do sleep 1; 
 done
 
