c8574822c1bdf33099cfdeb212da69423261bf03
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-12-13 10:01:02 +0200

    End-to-end tests for podcast deletion

--- src/database.py
+++ src/database.py
@@ -4,8 +4,5 @@ from config import DATABASE_URL
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL or \
     'sqlite:////tmp/test.db'
-    #if no DATABASE_URL given, sqlite database in-memory only
-    #if real file is needed for testing, replace above with below
-    #'sqlite://'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
