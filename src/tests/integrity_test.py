87be90c1bf74c196b88aec27c4f0c63d19d5a973
Timo Paananen <71272045+tspaanan@users.noreply.github.com>
2021-12-14 12:41:50 +0200

    Add integrity tests

---
+++ src/tests/integrity_test.py
@@ -0,0 +1,39 @@
+from services.book_service import BookService
+from services.podcast_service import PodcastService
+from services.weblink_service import WeblinkService
+from unittest import TestCase
+
+class TestIntegrity(TestCase):
+    book_service = BookService()
+    podcast_service = PodcastService()
+    weblink_service = WeblinkService()
+
+    def test_added_weblink_is_committed_to_database(self):
+        self.weblink_service.add_weblink('Integrity Weblink', 'http://in.te', 'No comment')
+        response = self.weblink_service.get_weblinks()
+        self.assertEqual(response[-1].title, 'Integrity Weblink')
+
+    def test_deleted_weblink_is_gone_from_database(self):
+        id = self.weblink_service.get_weblinks()[-1].id
+        self.weblink_service.delete_weblink(id)
+        self.assertNotEqual(self.weblink_service.get_weblinks()[-1].id, id)
+
+    def test_added_book_is_committed_to_database(self):
+        self.book_service.add_book('Integrity Book', 'Author', 2021, 'No comment')
+        response = self.book_service.get_books()
+        self.assertEqual(response[-1].title, 'Integrity Book')
+
+    def test_deleted_book_is_gone_from_database(self):
+        id = self.book_service.get_books()[-1].id
+        self.book_service.delete_book(id)
+        self.assertNotEqual(self.book_service.get_books()[-1].id, id)
+
+    def test_added_podcast_is_committed_to_database(self):
+        self.podcast_service.add_podcast('Integrity Podcast', 'Author Name', 'Description', 'No comment')
+        response = self.podcast_service.get_podcasts()
+        self.assertEqual(response[-1].title, 'Integrity Podcast')
+
+    def test_deleted_podcast_is_gone_from_database(self):
+        id = self.podcast_service.get_podcasts()[-1].id
+        self.podcast_service.delete_podcast(id)
+        self.assertNotEqual(self.podcast_service.get_podcasts()[-1].id, id)
